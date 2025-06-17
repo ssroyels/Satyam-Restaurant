437dd62b28fd0405769f4f6083c3b3fc64f8673b
ssroyels <satyam884060singh@gmail.com>
2025-06-17 09:26:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,10 +23,7 @@ app.use(cors({
 }));
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
+mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log("MongoDB connected");
 }).catch(err => {
   console.error("MongoDB connection error:", err);
